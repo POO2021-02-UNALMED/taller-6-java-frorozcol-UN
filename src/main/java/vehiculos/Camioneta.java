62c335f7c7d144e06df8ec5ada32dd13df24b5b5
Fredy <freorozcoloa@gmail.com>
2021-11-28 19:33:57 -0500

    * Los test tienen problemas cuando se ejecutan string

--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -5,7 +5,7 @@ public class Camioneta extends Vehiculo{
     private  boolean volco;
 
     public Camioneta(String placa, int puertas, String nombre, float precio, float peso, Fabricante fabricante, boolean volco) {
-        super(placa, puertas, 90, nombre, precio, peso,  "4x4", fabricante);
+        super(placa, puertas, 90, nombre, precio, peso,  "4X4", fabricante);
         this.volco = volco;
         cantidadCamioneta++;
     }
